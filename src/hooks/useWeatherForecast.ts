55cd67e9c26e0f5d54764087546a316ddfbce5e6
BogdanGrynjuk <bgrynjuk@ukr.net>
2024-05-10 03:11:53 +0300

    updates hook useWeatherForecast

--- src/hooks/useWeatherForecast.ts
+++ src/hooks/useWeatherForecast.ts
@@ -27,7 +27,6 @@ const useWeatherForecast = () => {
     try {
       if ('geolocation' in navigator) {
         const permissionStatus = await navigator.permissions?.query({ name: 'geolocation' });
-        alert(permissionStatus.state)
         if (permissionStatus?.state === 'granted') {
           navigator.geolocation.getCurrentPosition(function (position) {
             setLatitude(position.coords.latitude);
